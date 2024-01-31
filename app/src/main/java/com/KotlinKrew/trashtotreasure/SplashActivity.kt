dc7ff50068e95dd9e934fb30fc0730c2b4c247b4
guntursss <gunturmoch2008@gmail.com>
2024-01-31 08:39:06 +0700

    Baru lagi

--- app/src/main/java/com/KotlinKrew/trashtotreasure/SplashActivity.kt
+++ app/src/main/java/com/KotlinKrew/trashtotreasure/SplashActivity.kt
@@ -3,9 +3,9 @@ package com.KotlinKrew.trashtotreasure
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class NameActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_name)
+        setContentView(R.layout.activity_splash)
     }
 }
