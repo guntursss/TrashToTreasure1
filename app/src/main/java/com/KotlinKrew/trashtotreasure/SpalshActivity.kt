5141afb3c2a37b25f8b5c30cabf6db986a679f80
guntursss <gunturmoch2008@gmail.com>
2024-01-31 08:34:18 +0700

    Splash Screen

--- app/src/main/java/com/KotlinKrew/trashtotreasure/SpalshActivity.kt
+++ app/src/main/java/com/KotlinKrew/trashtotreasure/SpalshActivity.kt
@@ -1,11 +1,46 @@
 package com.KotlinKrew.trashtotreasure
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
-class SpalshActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
+    private lateinit var splashImage: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_activity)
+
+        splashImage = findViewById(R.id.iv_logo_splash_screen)
+
+        animateZoomOut()
+    }
+
+    private fun animateZoomOut() {
+        splashImage.animate()
+            .scaleX(0.4f)
+            .scaleY(0.4f)
+            .setDuration(1000)
+            .withEndAction {
+                animateZoomIn()
+            }
+            .start()
+    }
+
+    private fun animateZoomIn() {
+        splashImage.animate()
+            .scaleX(30f)
+            .scaleY(30f)
+            .setDuration(1000)
+            .withEndAction {
+                // mulai main activity
+                startNewActivity()
+            }
+            .start()
+    }
+
+    private fun startNewActivity() {
+        startActivity(Intent(this, NameActivity::class.java))
+        finish()
     }
 }
