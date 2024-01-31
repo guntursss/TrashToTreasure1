d4b1aa1132df4b8daae5029504e6a06a62344e89
guntursss <gunturmoch2008@gmail.com>
2024-01-31 08:45:54 +0700

    Baru lagi

--- app/src/main/java/com/KotlinKrew/trashtotreasure/SplashActivity.kt
+++ app/src/main/java/com/KotlinKrew/trashtotreasure/SplashActivity.kt
@@ -2,10 +2,20 @@ package com.KotlinKrew.trashtotreasure
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var splashImage:ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        splashImage = findViewById(R.id.iv_splash_screen_logo)
+
+        animateZoomOut()
+    }
+
+    private fun animateZoomOut() {
+        TODO("Not yet implemented")
     }
 }
