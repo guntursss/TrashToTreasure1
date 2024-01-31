5fa81127d284f4d8651cd5e6c2fb290ef4df6eb5
guntursss <gunturmoch2008@gmail.com>
2024-01-31 07:58:02 +0700

    Initial commit

---
+++ app/src/main/java/com/KotlinKrew/trashtotreasure/SpalshActivity.kt
@@ -0,0 +1,11 @@
+package com.KotlinKrew.trashtotreasure
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SpalshActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_activity)
+    }
+}
