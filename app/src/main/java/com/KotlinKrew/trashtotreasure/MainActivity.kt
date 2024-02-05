085b705df2aea4576189e7b17f9739bb2f3332d0
Abdullah <abdullahibniwan@gmail.com>
2024-02-05 09:49:23 +0700

    ada yang baru

--- app/src/main/java/com/KotlinKrew/trashtotreasure/MainActivity.kt
+++ app/src/main/java/com/KotlinKrew/trashtotreasure/MainActivity.kt
@@ -3,17 +3,28 @@ package com.KotlinKrew.trashtotreasure
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-   private lateinit var TombolPindah :Button
+    private lateinit var TombolPindah: Button
+    private lateinit var textInput:EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         TombolPindah = findViewById(R.id.btn_login)
+        textInput = findViewById(R.id.et_input_text)
         TombolPindah.setOnClickListener {
-            val pindah = Intent(this, HomeActivity::class.java)
-            startActivity(pindah)
+            TombolPindah.setOnClickListener {
+                if (TextUtils.isEmpty(textInput.text)) {
+                    Toast.makeText(this, "Harta tidak boleh kosong", Toast.LENGTH_LONG).show()
+                } else {
+                    val pindah = Intent(this, HomeActivity::class.java)
+                    startActivity(pindah)
+                }
+            }
         }
     }
 }
